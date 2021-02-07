34f605be033cd57c047c93c8dafd0973fc234bf7
Othmane Bakhtaoui <uo259323@uniovi.es>
2021-02-07 20:40:31 +0100

    travis i will die this time for real

--- rdfsync/util/__init__.py
+++ rdfsync/util/__init__.py
@@ -1,5 +1,4 @@
-from .errors import StringValidationError, FormatValidationError
-
 from .namespace_constants import default_rdf_namespaces
 
-from .string_util import get_namespace, get_triple_predicate_str, get_triple_subject_str, get_triple_object_str
+from .string_util import get_namespace, get_triple_predicate_str, get_triple_subject_str, get_triple_object_str, \
+    FormatValidationError, StringValidationError

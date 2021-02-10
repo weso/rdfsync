428d61a93923e0e61ad439d2bfc0e71be02870f3
Othmane Bakhtaoui <uo259323@uniovi.es>
2021-02-10 15:37:13 +0100

    replacing prints with loggers

--- rdfsync/wb2rdf/conversion.py
+++ rdfsync/wb2rdf/conversion.py
@@ -11,6 +11,7 @@ import logging
 logging.basicConfig()
 logger = logging.getLogger("conversion")
 
+
 class Converter:
     API_ENDPOINT = ''
     number_of_days = 0
@@ -125,17 +126,17 @@ class Converter:
         return claim_with_its_values
 
     def execute_synchronization(self, wb_id):
-        print('Sync in the wikibase <' + self.API_ENDPOINT + '>.')
+        logger.warning('Sync in the wikibase <' + self.API_ENDPOINT + '>.')
         pattern = re.compile(r'\s+')  # no spaces
         # subject info
         subject_rl = self.get_related_link_of_a_wb_item_or_property(wb_id)
         subject_name = re.sub(pattern, '', get_triple_subject_str(subject_rl))
         # name check
         if not subject_rl:
-            print('Please set related link of item/property with id <' + wb_id + '> in order to start the sync.')
+            logger.error('Please set related link of item/property with id <' + wb_id + '> in order to start the sync.')
             return
 
-        print('## sync of the subject <' + subject_name + '> ##')
+        logger.warning('## sync of the subject <' + subject_name + '> ##')
         # subjects to check it the graph contains it later.
         subjects_check = []
 
@@ -198,19 +199,19 @@ class Converter:
             # same lang of label, different values
             if label_lang in labels_of_subject_rdf.keys():
                 if str(labels_of_subject_wb[label_lang]) != str(labels_of_subject_rdf[label_lang]):
-                    print('same language of label of <' + subject_name + '> but with different values')
+                    logger.warning('same language of label of <' + subject_name + '> but with different values')
                     self.graph.set(
                         (URIRef(subject_rl), RDFS.label, Literal(labels_of_subject_wb[label_lang], lang=label_lang)))
             # new lang of label not in rdf but is in wb
             else:
-                print('new language of label of <' + subject_name + '> not in rdf but is in wb')
+                logger.warning('new language of label of <' + subject_name + '> not in rdf but is in wb')
                 self.graph.add(
                     (URIRef(subject_rl), RDFS.label, Literal(labels_of_subject_wb[label_lang], lang=label_lang)))
         # deleting labels if not exists in wb but exists in rdf
         for label_lang in labels_of_subject_rdf.keys():
             if label_lang not in labels_of_subject_wb.keys():
-                print('deleting label of the language <' + label_lang + '> of <'
-                      + subject_name + '> because it does not exist in wb')
+                logger.warning('deleting label of the language <' + label_lang + '> of <'
+                               + subject_name + '> because it does not exist in wb')
                 self.graph.remove(
                     (URIRef(subject_rl), RDFS.label, Literal(labels_of_subject_rdf[label_lang], lang=label_lang)))
         # _______________________                  ____________________#
@@ -218,26 +219,27 @@ class Converter:
         # _______________________ DESCRIPTIONS ____________________#
         # comparing descriptions
         if not bool(descriptions_of_subject_wb):
-            print('there are no descriptions in wb. updating the rdf')
+            logger.info('there are no descriptions in wb. updating the rdf')
             self.graph.remove((URIRef(subject_rl), RDFS.comment, None))
         else:
             for descr_lang in descriptions_of_subject_wb.keys():
                 # same lang of description, different values
                 if descr_lang in descriptions_of_subject_rdf.keys():
                     if str(descriptions_of_subject_wb[descr_lang]) != str(descriptions_of_subject_rdf[descr_lang]):
-                        print('same language of description of <' + subject_name + '> but with different values')
+                        logger.warning(
+                            'same language of description of <' + subject_name + '> but with different values')
                         self.graph.set((URIRef(subject_rl), RDFS.comment,
                                         Literal(descriptions_of_subject_wb[descr_lang], lang=descr_lang)))
                 # new lang of description not in rdf but is in wb
                 else:
-                    print('new language of description of <' + subject_name + '> not in rdf but is in wb')
+                    logger.info('new language of description of <' + subject_name + '> not in rdf but is in wb')
                     self.graph.add((URIRef(subject_rl), RDFS.comment,
                                     Literal(descriptions_of_subject_wb[descr_lang], lang=descr_lang)))
         # deleting descriptions if not exists in wb but exists in rdf
         for descr_lang in descriptions_of_subject_rdf.keys():
             if descr_lang not in descriptions_of_subject_wb.keys():
-                print('deleting description of the language <' + descr_lang + '> of <'
-                      + subject_name + '> because it does not exist in wb')
+                logger.info('deleting description of the language <' + descr_lang + '> of <'
+                            + subject_name + '> because it does not exist in wb')
                 self.graph.remove(
                     (URIRef(subject_rl), RDFS.comment,
                      Literal(descriptions_of_subject_rdf[descr_lang], lang=descr_lang)))
@@ -246,14 +248,15 @@ class Converter:
         # _______________________ PREDICATES and OBJECTS / CLAIMS ____________________#
         # comparing predicates
         if set(claims_of_wb_item) != set(predicates_of_subject):
-            print('different predicates in the item/subject <' + subject_name + '> with wikibase ID <' + wb_id + '>')
+            logger.info(
+                'different predicates in the item/subject <' + subject_name + '> with wikibase ID <' + wb_id + '>')
 
             # deletion: predicate exists in rdf but is not in wb.
             set_of_predicates = set(predicates_of_subject)
             predicates_in_rdf_not_in_wb = [x for x in set_of_predicates if x not in claims_of_wb_item]
             if len(predicates_in_rdf_not_in_wb) != 0:
                 for predicate_to_delete in predicates_in_rdf_not_in_wb:
-                    print('deletion of <' + str(get_triple_predicate_str(
+                    logger.warning('deletion of <' + str(get_triple_predicate_str(
                         predicate_to_delete)) + '> in rdf because predicate exists in rdf but is not in wb.')
                     self.graph.remove((URIRef(subject_rl), URIRef(predicate_to_delete), None))
 
@@ -262,27 +265,28 @@ class Converter:
             predicates_in_wb_not_in_rdf = [x for x in set_of_claims if x not in predicates_of_subject]
             if len(predicates_in_wb_not_in_rdf) != 0:
                 for predicate_to_add in predicates_in_wb_not_in_rdf:
-                    print('addition of <' + str(get_triple_predicate_str(
+                    logger.warning('addition of <' + str(get_triple_predicate_str(
                         predicate_to_add)) + '> in rdf because predicate exists in wb but is not in rdf.')
                     for value_of_claim in claim_and_value_dictionary[predicate_to_add]:
                         self.graph.add((URIRef(subject_rl), URIRef(predicate_to_add),
                                         URIRef(value_of_claim)))
         else:
-            print('same predicates in the item/subject <' + subject_name + '> with wikibase ID <' + wb_id + '>')
+            logger.info('same predicates in the item/subject <' + subject_name + '> with wikibase ID <' + wb_id + '>')
 
-        print('comparing the objects in rdf to claim values in wikibase')
+        logger.info('comparing the objects in rdf to claim values in wikibase')
         if claim_and_value_dictionary == predicate_and_object_dictionary:
-            print('same objects in the item/subject <' + subject_name + '> with wikibase ID <' + wb_id + '>')
+            logger.info('same objects in the item/subject <' + subject_name + '> with wikibase ID <' + wb_id + '>')
         else:
-            print('detecting if different objects in the item/subject <' + subject_name
-                  + '> with wikibase ID <' + wb_id + '>')
+            logger.info('detecting if different objects in the item/subject <' + subject_name
+                        + '> with wikibase ID <' + wb_id + '>')
             for predicate_to_update in claim_and_value_dictionary.keys():
                 # updating the graph with the new value of the object
                 if predicate_to_update in predicate_and_object_dictionary.keys():
                     if str(predicate_and_object_dictionary[predicate_to_update]) != str(
                             claim_and_value_dictionary[predicate_to_update]):
-                        print('updating object of the predicate <' + predicate_to_update + '>in the item/subject <'
-                              + subject_name + '> with wikibase ID <' + wb_id + '>')
+                        logger.warning(
+                            'updating object of the predicate <' + predicate_to_update + '>in the item/subject <'
+                            + subject_name + '> with wikibase ID <' + wb_id + '>')
                         # removing old object
                         self.graph.remove((URIRef(subject_rl), URIRef(predicate_to_update), None))
                         # updating with new value
@@ -291,25 +295,25 @@ class Converter:
 
     def create_new_triple(self, claim_and_value_dictionary, descriptions_of_subject_wb, wb_id, labels_of_subject_wb,
                           subject_name, subject_rl):
-        print('The wb item/property with ID <' + wb_id + '> does not exist in rdf file.')
-        print('Creating a new triple with its data.')
+        logger.warning('The wb item/property with ID <' + wb_id + '> does not exist in rdf file.')
+        logger.info('Creating a new triple with its data.')
         # adding new namespaces if they doesn't exist
         self.binding_namespace_of_graph(subject_rl)
         # new labels
         for label_lang in labels_of_subject_wb.keys():
-            print('new language of label of <' + subject_name + '> CREATED')
+            logger.info('new language of label of <' + subject_name + '> CREATED')
             self.graph.add((URIRef(subject_rl), RDFS.label, Literal(labels_of_subject_wb[label_lang], lang=label_lang)))
         # new descriptions
         for descr_lang in descriptions_of_subject_wb.keys():
-            print('new language of description of <' + subject_name + '> CREATED')
+            logger.info('new language of description of <' + subject_name + '> CREATED')
             self.graph.add((URIRef(subject_rl), RDFS.comment,
                             Literal(descriptions_of_subject_wb[descr_lang], lang=descr_lang)))
         # new triples
         for new_predicate in claim_and_value_dictionary.keys():
             # adding new namespaces if they doesn't exist
             self.binding_namespace_of_graph(new_predicate)
-            print('new predicate <' + new_predicate + '> for the item/subject <' + subject_name
-                  + '> with wikibase ID <' + wb_id + '>')
+            logger.warning('new predicate <' + new_predicate + '> for the item/subject <' + subject_name
+                           + '> with wikibase ID <' + wb_id + '>')
             for value_of_claim in claim_and_value_dictionary[str(new_predicate)]:
                 # adding new namespaces if they doesn't exist
                 self.binding_namespace_of_graph(value_of_claim)
@@ -331,7 +335,7 @@ class Converter:
             wb_id = poi.text.split(':')[1]
             to_update_list.add(wb_id)
         final_sync_list = set(to_update_list)
-        print('The items/properties to sync are: ' + str(final_sync_list))
+        logger.info('The items/properties to sync are: ' + str(final_sync_list))
         return final_sync_list
 
     def serialize_file(self, output_format='ttl'):

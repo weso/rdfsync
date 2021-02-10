3bf16c2a928a0605db2784a0c14b9f4cd63837ed
Othmane Bakhtaoui <uo259323@uniovi.es>
2021-02-10 17:37:53 +0100

    more label and descr tests

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 [![Codacy Badge](https://app.codacy.com/project/badge/Grade/6df235099f9b4dd5816551e6c82d432a)](https://www.codacy.com/gh/weso/rdfsync/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=weso/rdfsync&amp;utm_campaign=Badge_Grade)
 [![License](https://img.shields.io/github/license/weso/rdfsync)](https://github.com/weso/rdfsync/blob/master/LICENSE)
 
+
 An algorithm to synchronise data between the ontology files and a given Wikibase instance.
 
 ## How to synchronize 
--- tests/test_update_label_description.py
+++ tests/test_update_label_description.py
@@ -7,7 +7,7 @@ graph.parse("tests/data/synchronization/uld.ttl", format='ttl')
 converter = Converter(endpoint=MEDIAWIKI_API_URL, input_format='ttl', graph=graph)
 wikibase_id_1 = 'Q4'  # human resource
 wikibase_id_2 = 'Q11'  # country
-
+wikibase_id_3 = 'Q12'  # student
 # stay the same
 subject_1_label = ((URIRef('http://www.purl.org/hercules/asio/core#HumanResource'),
                     URIRef('http://www.w3.org/2000/01/rdf-schema#label'),
@@ -44,6 +44,22 @@ subject_2_new_description = ((URIRef('http://www.purl.org/hercules/asio/core#cou
                                   'The domain is not set so unpredicted resources within the ontology '
                                   'could be attached to countries.', lang='en')))
 
+subject_3_old_triple_1 = ((URIRef('http://www.purl.org/hercules/asio/core#student'),
+                           URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+                           URIRef('http://www.w3.org/2002/07/owl#Class')))
+
+subject_3_old_triple_2 = ((URIRef('http://www.purl.org/hercules/asio/core#student'),
+                           URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+                           URIRef('http://www.w3.org/2002/07/owl#ObjectProperty')))
+
+subject_3_old_comment = ((URIRef('http://www.purl.org/hercules/asio/core#student'),
+                          URIRef('http://www.w3.org/2000/01/rdf-schema#comment'),
+                          Literal('This will be deleted', lang='en')))
+
+subject_3_label = ((URIRef('http://www.purl.org/hercules/asio/core#student'),
+                    URIRef('http://www.w3.org/2000/01/rdf-schema#label'),
+                    Literal('student', lang='en')))
+
 
 def test_populate_empty_rdf():
     assert not graph.__contains__(subject_2_new_description)
@@ -66,4 +82,21 @@ def test_populate_empty_rdf():
     assert graph.__contains__(subject_1_comment)
 
 
-graph.serialize()
+def test_populate_empty_rdf():
+    graph = Graph()
+    graph.parse("tests/data/synchronization/uld2.ttl", format='ttl')
+    assert not graph.__contains__(subject_1_label)  # non existent
+    assert graph.__contains__(subject_3_old_comment)
+    assert graph.__contains__(subject_3_label)
+    assert graph.__contains__(subject_3_old_triple_1)
+    assert graph.__contains__(subject_3_old_triple_2)
+    assert len(graph) == 4
+
+    converter2 = Converter(endpoint=MEDIAWIKI_API_URL, input_format='ttl', graph=graph)
+    converter2.execute_synchronization(wb_id=wikibase_id_3)
+
+    assert not graph.__contains__(subject_1_label)  # non existent
+    assert not graph.__contains__(subject_3_old_comment)  # deleted
+    assert graph.__contains__(subject_3_label)
+    assert not graph.__contains__(subject_3_old_triple_1)  # deleted
+    assert not graph.__contains__(subject_3_old_triple_2)  # deleted
